b6909a9f1cc2dc8e00d4967a5b79be248c176f78
Robert Gistvik <8BitKnitNerd@users.noreply.github.com>
2017-05-09 16:07:44 +0200

    Add files via upload

--- SeleniumAutotestDotNet/Pages/SearchResultPage.cs
+++ SeleniumAutotestDotNet/Pages/SearchResultPage.cs
@@ -13,6 +13,14 @@ namespace SeleniumAutotestDotNet.Pages {
 
         private IWebElement SearchResults => Page.FindElement(By.Id("resultStats"));
 
+        private IWebElement PreviousButton => Page.FindElement(By.Id("pnprev"));
+
+        private IWebElement NextButton => Page.FindElement(By.Id("pnnext"));
+
+        private IWebElement CurrentPage => Page.FindElement(By.ClassName("cur"));
+
+        private ReadOnlyCollection<IWebElement> PaginationNumbers => Page.FindElement(By.Id("nav")).FindElements(By.TagName("td"));
+
         private ReadOnlyCollection<IWebElement> PageResults => Page.FindElements(By.ClassName("g"));
 
         public SearchResultPage(WebDriver inBrowser) {
@@ -25,6 +33,31 @@ namespace SeleniumAutotestDotNet.Pages {
 
         public int GetNumberOfResults() {
             return TestHelper.GetIntFromString(SearchResults.Text.Split(new string[] { " resultat" }, StringSplitOptions.None)[0]);
+        }
+
+        public void PaginateNext() {
+            NextButton.Click();
+            Browser.WaitForPageAndAjax();
+        }
+
+        public void PaginatePrevious()
+        {
+            PreviousButton.Click();
+            Browser.WaitForPageAndAjax();
+        }
+
+        public void PaginateToNumber(int pageNumber) {
+            foreach(IWebElement number in PaginationNumbers) {
+                if(number.Text == pageNumber.ToString()) {
+                    number.Click();
+                    Browser.WaitForPageAndAjax();
+                    break;
+                }
+            }
+        }
+
+        public int GetCurrentPage() {
+            return TestHelper.GetIntFromString(CurrentPage.Text);
         }
 
         public bool PageIsLoaded() {
